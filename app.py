a64ea1e56283704e5bce638d2b22212385994002
czett <keinaniemand@gmail.com>
2024-09-11 18:41:41 +0200

    gitignore

---
+++ app.py
@@ -0,0 +1,20 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+with open("credentials.yml", "r") as creds:
+    key = creds.readline()
+
+print(key)
+
+uri = f"mongodb+srv://germannhugo:{key}@cluster0.bwmg4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
